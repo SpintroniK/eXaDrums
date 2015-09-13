15d5fa622975ca45ea6cbc6613a75051f0be2e73
Jeremy <jerem.oden@gmail.com>
2015-09-13 12:25:39 +0100

    Working test for the hdd sensor.

--- Source/Testing/eXaDrumsHddTest.cpp
+++ Source/Testing/eXaDrumsHddTest.cpp
@@ -8,9 +8,12 @@
 #include "eXaDrumsHddTest.h"
 
 #include <Source/Api/eXaDrumKit.h>
+#include <Source/IO/HddSensor.h>
 
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
 
 using namespace eXaDrumKitApi;
 
@@ -21,14 +24,35 @@ namespace Testing
 	{
 
 		std::string moduleLocation("/home/jeremy/Desktop/Prog/eXaDrums/eXaDrums/Data/");
-
 		eXaDrumKit drumKit(moduleLocation.c_str());
 
 		std::string kitLocation("Kits/default.xml");
-
 		drumKit.LoadKit(kitLocation.c_str());
 
-		std::cout << "Test" << std::endl;
+		// Create Hdd sensor
+		std::string sensorFile(moduleLocation + "../out.raw");
+		IO::HddSensor hddSensor(sensorFile.c_str());
+
+		// Add a trigger for drum id 0
+		const int snareId = 0;
+		drumKit.AddTrigger(snareId);
+
+		// Start drum module
+		drumKit.Start();
+
+		int N = hddSensor.GetDataLength();
+
+
+		for(int i = 0; i < N; i++)
+		{
+
+			short value = hddSensor.GetOutput();
+			drumKit.Trig(snareId, value);
+
+		}
+
+		// Stop drum module
+		drumKit.Stop();
 
 		return;
 	}
--- Source/main.cpp
+++ Source/main.cpp
@@ -11,7 +11,6 @@
 int main(int argc, char* argv[])
 {
 
-
 	Testing::eXaDrumsHddTest::RunTest();
 
 	return 0;
