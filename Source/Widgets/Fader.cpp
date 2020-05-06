ba87d49f8db0908261a41c7c5816a3b4f7de817b
Jeremy Oden <jerem.oden@gmail.com>
2020-05-06 12:06:24 +0100

    Faders labels are now aligned left.

--- Source/Widgets/Fader.cpp
+++ Source/Widgets/Fader.cpp
@@ -20,6 +20,7 @@ namespace Widgets
 		// Set title
 		label.set_halign(Gtk::Align::ALIGN_START);
 		label.set_justify(Gtk::Justification::JUSTIFY_LEFT);
+		label.set_xalign(0.f);
 		label.set_size_request(72, -1);
 		label.show();
 		this->attach(label, 0, 0, 1, 1);
