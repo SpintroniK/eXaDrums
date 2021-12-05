8f7f7ad099a9f95896cc3be19d0c71d18c8711c7
Jeremy Oden <jerem.oden@gmail.com>
2021-12-05 14:44:38 +0100

    Improve SpiDev widget.

--- Source/Widgets/SpiDev.h
+++ Source/Widgets/SpiDev.h
@@ -4,7 +4,8 @@
 #include <libexadrums/Api/Config/SpiDevParameters_api.h>
 
 #include <gtkmm/grid.h>
-#include <gtkmm/entry.h>
+#include <gtkmm/spinbutton.h>
+#include <gtkmm/adjustment.h>
 
 #include <string>
 
@@ -17,10 +18,21 @@ namespace Widgets
         SpiDev(const eXaDrumsApi::SpiDevParameters& params)
         {
             name.set_text(params.name);
-            bus.set_text(std::to_string(params.bus));
+            
+            cs.get_adjustment()->set_value(params.cs);
+            bus.get_adjustment()->set_value(params.bus);
+
+            this->set_column_spacing(4);
+            this->set_row_spacing(4);
+
+            this->set_margin_left(4);
+            this->set_margin_right(4);
+            this->set_margin_top(4);
+            this->set_margin_bottom(4);
 
             this->attach(name, 0, 0, 1, 1);
             this->attach_next_to(bus, name, Gtk::PositionType::POS_RIGHT, 1, 1);
+            this->attach_next_to(cs, bus, Gtk::PositionType::POS_RIGHT, 1, 1);
 
             this->show_all();
         }
@@ -28,7 +40,12 @@ namespace Widgets
     private:
 
         Gtk::Entry name{};
-        Gtk::Entry bus{};
+
+        Glib::RefPtr<Gtk::Adjustment> busAdj{Gtk::Adjustment::create(0, 0, 10)};
+        Gtk::SpinButton bus{busAdj};
+
+        Glib::RefPtr<Gtk::Adjustment> csAdj{Gtk::Adjustment::create(0, 0, 10)};
+        Gtk::SpinButton cs{csAdj};
         
     };
 
